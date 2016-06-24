08eaa110da02c016607a271e6bef024c14925c67
Monika <monika_jovanova@hotmail.com>
2016-06-25 00:09:13 +0200

    Added Database
    Added Database and added database migrations

--- IT-Core/Plane.cs
+++ IT-Core/Plane.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace IT_Core
 {
-    class Plane:Base
+    public class Plane:Base
     {
         public string SerialNum { get; set; }
         public int Capacity { get; set; }
